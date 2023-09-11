c39628af75447f45d145bc86c2d7e52c2f9debdb
FMVF77C2B\Owner <pgakatuki1234@gmail.com>
2023-09-11 17:42:46 +0900

    theme file first backup

---
+++ functions.php
@@ -0,0 +1,80 @@
+<?php
+##===================== テーマのセットアップ =====================##
+/**
+ * テーマのセットアップを実行。
+ * 
+ * @method       setup_theme
+ * @return null  返り値なし。
+ */
+function setup_theme()
+{
+
+}
+
+add_action( 'after_setup_theme', 'setup_theme' );
+
+##===============================================================##
+
+
+##===================== header =====================##
+/**
+ * 「外観」に「メニュー」を追加する。
+ * 
+ * @method      add_apparence_to_menu
+ * @param null  返り値なし。
+ */
+function add_apparence_to_menu()
+{
+  register_nav_menus( array(
+    'header-nav' => 'header-navigation',
+    'header-nav-menu-collects' => 'header-menu-collects-navgation',
+    'footer-nav' => 'footer-navigation',
+  ));  
+};
+
+add_action( 'after_setup_theme', 'add_apparence_to_menu' );
+
+##==================================================##
+
+##============== CSS ファイル読み込み ==============##
+
+function load_for_css_file() {
+/**
+ * cdn で destyle.css 読み込み。 
+ * 
+ * @method       load_for_css_file
+ * @return null  返り値なし。
+ */
+  $css_destyle_uri = 'https://cdn.jsdelivr.net/npm/destyle.css@1.0.15/destyle.css';
+  wp_enqueue_style('cdn-destyle', $css_destyle_uri, array(), 'all');
+
+  
+/**
+ * style.css 読み込み。 
+ * 
+ * @method       load_for_css_file
+ * @return null  返り値なし。
+ */
+  $css_style_uri = get_template_directory_uri().'/style.css';
+  wp_enqueue_style('css-style', $css_style_uri, array(), 'all');
+
+};
+
+add_action( 'after_setup_theme', 'load_for_css_file' );
+
+##==================================================##
+
+
+##=========== JavaScript ファイル読み込み ===========##
+/**
+ * jquery 読み込み。
+ * 
+ * @method       load_for_javascript_file
+ * @return null  返り値なし。
+ */
+function load_for_javascript_file() {}
+
+##===================================================##
+
+
+?>
---
+++ header.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="<?php bloginfo( 'language' ) ?>">
+<head>
+  <meta charset="<?php bloginfo( 'charset' ) ?>">
+  <title><?php bloginfo( 'name' ) ?></title>
+  <meta name='viewport' content='width=device-width, initial-scale=1'>
+  <link rel="icon" href="./img/favicon.ico" type="image/x-icon">
+  <?php wp_head(); ?>
+</head>
+<body>
+  <div>
+    <header>
+      <h2><?php echo bloginfo('name') ?></h2>
+    </header>
